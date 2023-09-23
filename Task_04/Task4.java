621db24242cf52e32b9c1e740e78fd0918724b68
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-09-23 11:31:07 +0530

    Added Task 4 & 5

---
+++ Task_04/Task4.java
@@ -0,0 +1,57 @@
+import javax.crypto.*;
+import java.util.*;
+
+class MyDES {
+
+    Cipher eCipher;
+    Cipher dCipher;
+
+    //initialization
+    MyDES(SecretKey key) throws Exception {
+        eCipher = Cipher.getInstance("AES");
+        dCipher = Cipher.getInstance("AES");
+        eCipher.init(Cipher.ENCRYPT_MODE, key);
+        dCipher.init(Cipher.DECRYPT_MODE, key);
+    }
+
+    public String encrypt(String str) throws Exception {
+        byte[] byteData = str.getBytes("UTF8");
+        //doFinal expects type of byte[] and returns byte[]
+        byte[] enc = eCipher.doFinal(byteData);
+
+        // Encode bytes to base64 to get a string
+        return Base64.getEncoder().encodeToString(enc);
+    }
+
+    public String decrypt(String str) throws Exception {
+        byte[] dec = Base64.getDecoder().decode(str);
+        //doFinal expects type of byte[] and returns byte[]
+        byte[] byteData = dCipher.doFinal(dec);
+
+        return new String(byteData, "UTF8");
+    }
+}
+
+class Task4{
+    public static void main(String[] argv) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter text to ecrypt: ");
+        final String secretText = sc.next();
+        System.out.println("SecretText: " + secretText);
+
+        // generation of Secret key
+        SecretKey key = KeyGenerator.getInstance("AES").generateKey();
+        
+        // Instantiation of our custom class
+        MyDES myDES = new MyDES(key);
+
+        String encrypted = myDES.encrypt(secretText);
+        System.out.println("Encrypted Value: " + encrypted);
+
+        String decrypted = myDES.decrypt(encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        sc.close();
+    }
+}
---
+++ Task_05/RC4.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class RC4 {
+    public static void main(String args[]) {
+        int temp = 0;
+        int s[] = new int[256];
+        int k[] = new int[256];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n ENTER PLAIN TEXT\t");
+        String ptext = sc.next();
+        System.out.println("\n\nENTER KEY TEXT\t\t");
+        String key = sc.next();
+        
+        char ptextc[] = ptext.toCharArray();
+        char keyc[] = key.toCharArray();
+        int cipher[] = new int[ptext.length()];
+        int decrypt[] = new int[ptext.length()];
+        int ptexti[] = new int[ptext.length()];
+        int keyi[] = new int[key.length()];
+
+        for (int i = 0; i < ptext.length(); i++) 
+            ptexti[i] = (int) ptextc[i];
+        
+        for (int i = 0; i < key.length(); i++) 
+            keyi[i] = (int) keyc[i];
+        
+        for (int i = 0; i < 255; i++) {
+            s[i] = i;
+            k[i] = keyi[i % key.length()];
+        }
+        int j = 0;
+        for (int i = 0; i < 255; i++) {
+            j = (j + s[i] + k[i]) % 256;
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+        int i = 0;
+        j = 0;
+        int z = 0;
+        for (int l = 0; l < ptext.length(); l++) {
+            i = (l + 1) % 256;
+            j = (j + s[i]) % 256;
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            z = s[(s[i] + s[j]) % 256];
+            cipher[l] = z ^ ptexti[l];
+            decrypt[l] = z ^ cipher[l];
+        }
+        System.out.println("\n\nENCRYPTED:\t\t");
+        display(cipher);
+        System.out.println("\n\nDECRYPTED:\t\t");
+        display(decrypt);
+    }
+
+    static void display(int disp[]) {
+        char convert[] = new char[disp.length];
+        for (int l = 0; l < disp.length; l++) {
+            convert[l] = (char) disp[l];
+            System.out.print(convert[l]);
+        }
+    }
+
+}
