ac42347ca2265e03990053463b24aab325b738fa
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-11-16 23:02:32 +0530

    Refractored Task 10

--- Task_10/Task10.java
+++ Task_10/Task10.java
@@ -1,23 +1,26 @@
 import java.math.BigInteger;
 import java.security.*;
 
-public class Task10{
+public class Task10 {
 	public static void main(String[] args) {
 		System.out.println("For null " + md5(""));
-		System.out.println("For simple text "+ md5("This is my text"));
+		System.out.println("For simple text " + md5("This is my text"));
 		System.out.println("For simple numbers " + md5("12345"));
 	}
+
 	public static String md5(String input) {
-		String md5 = null;
-		if(null == input) return null;
+		if (input == null)
+			return null;
 		try {
 			MessageDigest digest = MessageDigest.getInstance("MD5");
-			digest.update(input.getBytes(), 0, input.length());
-			md5 = new BigInteger(1, digest.digest()).toString(16);
-		}
-		catch (NoSuchAlgorithmException e) {
+			digest.update(input.getBytes());
+
+			// 1-> positive number 16-> for hexa to string
+			String md5 = new BigInteger(1, digest.digest()).toString(16);
+			return md5;
+		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		return md5;
+		return null;
 	}
 }
