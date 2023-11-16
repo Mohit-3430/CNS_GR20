c742560a5efd61beb65f00f634bbdc4ea25a245c
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-11-16 22:41:21 +0530

    Refractored Task 8 according to the readme file notations

--- Task_08/Task8.java
+++ Task_08/Task8.java
@@ -5,28 +5,29 @@ public class Task8 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        //pub key
         System.out.println("Enter prime number:");
         BigInteger p = new BigInteger(br.readLine());
-
+        //pub key
         System.out.print("Enter primitive root of " + p + ":");
         BigInteger g = new BigInteger(br.readLine());
+        //priv key
+        System.out.println("Enter value for a less than " + p + ":");
+        BigInteger a = new BigInteger(br.readLine());
+        //key generated
+        BigInteger x = g.modPow(a, p);
+        System.out.println("R1=" + x);
+        // priv Key
+        System.out.print("Enter value for b less than " + p + ":");
+        BigInteger b = new BigInteger(br.readLine());
+        //key generated
+        BigInteger y = g.modPow(b, p);
+        System.out.println("R2=" + y);
 
-        System.out.println("Enter value for x less than " + p + ":");
-        BigInteger x = new BigInteger(br.readLine());
-
-        BigInteger R1 = g.modPow(x, p);
-        System.out.println("R1=" + R1);
-    
-        System.out.print("Enter value for y less than " + p + ":");
-        BigInteger y = new BigInteger(br.readLine());
-        
-        BigInteger R2 = g.modPow(y, p);
-        System.out.println("R2=" + R2);
-
-        BigInteger k1 = R2.modPow(x, p);
+        BigInteger k1 = y.modPow(a, p);
         System.out.println("Key1 calculated :" + k1);
 
-        BigInteger k2 = R1.modPow(y, p);
+        BigInteger k2 = x.modPow(b, p);
         System.out.println("Key2 calculated :" + k2);
         System.out.println("deffie hellman secret key Encryption has Taken");
     }
