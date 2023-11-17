f880da98d848c44e38784d795b8700c7472cde4c
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-11-17 21:23:06 +0530

    refractored code for 8

--- Task_08/Task8.java
+++ Task_08/Task8.java
@@ -5,24 +5,24 @@ public class Task8 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //pub key
+        // pub key
         System.out.println("Enter prime number:");
         BigInteger p = new BigInteger(br.readLine());
-        //pub key
+        // pub key
         System.out.print("Enter primitive root of " + p + ":");
         BigInteger g = new BigInteger(br.readLine());
-        //priv key
+        // priv key
         System.out.println("Enter value for a less than " + p + ":");
         BigInteger a = new BigInteger(br.readLine());
-        //key generated
+        // key generated
         BigInteger x = g.modPow(a, p);
-        System.out.println("R1=" + x);
+        System.out.println("x=" + x);
         // priv Key
         System.out.print("Enter value for b less than " + p + ":");
         BigInteger b = new BigInteger(br.readLine());
-        //key generated
+        // key generated
         BigInteger y = g.modPow(b, p);
-        System.out.println("R2=" + y);
+        System.out.println("y=" + y);
 
         BigInteger k1 = y.modPow(a, p);
         System.out.println("Key1 calculated :" + k1);
