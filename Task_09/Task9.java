9521af61b2882c7164b8402524bc3afa11308e45
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-11-17 21:41:56 +0530

    Made 9th task similar to 10

--- Task_09/Task9.java
+++ Task_09/Task9.java
@@ -1,34 +1,57 @@
 
+import java.math.BigInteger;
 import java.security.*;
 import java.util.*;
 
 public class Task9 {
     public static void main(String[] a) {
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.next();
+        System.out.println(sha1(inp));
+    }
+
+    public static String sha1(String input) {
+        if (input == null)
+            return null;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA1");
-            System.out.println("Message digest object info:");
+            MessageDigest digest = MessageDigest.getInstance("SHA1");
+            digest.update(input.getBytes());
 
-            System.out.println("Algorithm=" + md.getAlgorithm());
-            System.out.println("Provider=" + md.getProvider());
-            System.out.println("ToString=" + md.toString());
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the pt:");
-            String input = sc.next();
-
-            md.update(input.getBytes());
-            byte[] output = md.digest();
-            StringBuilder sb = new StringBuilder();
-            for (byte b : output) {
-                sb.append(String.format("%02x", b));
-            }
-
-            System.out.println("Hashed Output: " + sb.toString());
-            // input = "abc";
-            // input = "abcdefghijklmnopqrstuvwxyz";
-
-        } catch (Exception e) {
-            System.out.println("Exception:" + e);
+            // 1-> positive number 16-> for hexa to string
+            String sha1 = new BigInteger(1, digest.digest()).toString(16);
+            return sha1;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
+
+// =============== old code ===========================
+// try {
+// MessageDigest md = MessageDigest.getInstance("SHA1");
+// System.out.println("Message digest object info:");
+//
+// System.out.println("Algorithm=" + md.getAlgorithm());
+// System.out.println("Provider=" + md.getProvider());
+// System.out.println("ToString=" + md.toString());
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the pt:");
+// String input = sc.next();
+// System.out.println(sha1(input));
+
+// md.update(input.getBytes());
+// byte[] output = md.digest();
+// StringBuilder sb = new StringBuilder();
+// for (byte b : output) {
+// sb.append(String.format("%02x", b));
+// }
+
+// System.out.println("Hashed Output: " + sb.toString());
+// //input = "abc";
+// //input = "abcdefghijklmnopqrstuvwxyz";
+
+// } catch (Exception e) {
+// System.out.println("Exception:" + e);
+// }
