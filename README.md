34a3ba834ca10b71ad134969959e7aba8d89ce88
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-12-20 23:08:13 +0530

    Refractored Task 5 and added description in README

--- README.md
+++ README.md
@@ -1,18 +1,52 @@
+</details>
+
+<details>
+
+<summary>Task 5</summary>
+
+### RC4
+
+Working of RC4
+
+#### Main Things:
+
+1. Key Scheduling
+2. Key Stream Generation
+3. Encryption and Decryption
+
+#### Encryption Procedure
+
+The user inputs a plain text file and a secret key.
+The encryption engine then generates the keystream by using KSA and PRGA Algorithm.
+This keystream is now XOR with the plain text, this XORing is done byte by byte to produce the encrypted text.
+The encrypted text is then sent to the intended receiver, the intended receiver will then decrypted the text and after decryption, the receiver will get the original plain text.
+
+#### Decryption Procedure
+
+Decryption is achieved by doing the same byte-wise X-OR operation on the Ciphertext.
+
+</details>
+
 <details>
 <summary>Task 7</summary>
 
 ### RSA
+
 - Select two large prime numbers, p and q.
 - n = p x q, where n is called the modulus for encryption and decryption.
-- Φ(n) = (P-1)(Q-1) 
+- Φ(n) = (P-1)(Q-1)
 - Choose value of **e** such that 1<e<n and gcd(Φ(n), e) = 1;
 - calculate d = e <sup>-1</sup> modΦ(n) => e d modΦ(n) = 1
 - pubK = {e, n}
 - privK = {d, n}
- ##### ENC
- C = M<sup>e</sup>modN
- ##### DEC
- M = C<sup>d</sup>modN
+
+##### ENC
+
+C = M<sup>e</sup>modN
+
+##### DEC
+
+M = C<sup>d</sup>modN
 
 </details>
 
@@ -31,6 +65,3 @@
 |Generated Secret Key =k_a = y<sup>a</sup> mod P| Generated Secret Key =k_b = x<sup>b</sup> mod P|
 |Algebraically, it can be shown thatk_a = k_b|
 |Users now have a symmetric secret key to encrypt|
-
-  
-</details>
--- Task_05/RC4.java
+++ Task_05/RC4.java
@@ -3,32 +3,33 @@ import java.util.*;
 class RC4 {
     public static void main(String args[]) {
         int temp = 0;
-        int s[] = new int[256];
-        int k[] = new int[256];
+        int s[] = new int[256]; // state vector
+        int k[] = new int[256]; // key array
 
         Scanner sc = new Scanner(System.in);
         System.out.println("\n ENTER PLAIN TEXT\t");
         String ptext = sc.next();
         System.out.println("\n\nENTER KEY TEXT\t\t");
         String key = sc.next();
-        
-        char ptextc[] = ptext.toCharArray();
-        char keyc[] = key.toCharArray();
-        int cipher[] = new int[ptext.length()];
-        int decrypt[] = new int[ptext.length()];
+
         int ptexti[] = new int[ptext.length()];
         int keyi[] = new int[key.length()];
 
-        for (int i = 0; i < ptext.length(); i++) 
-            ptexti[i] = (int) ptextc[i];
-        
-        for (int i = 0; i < key.length(); i++) 
-            keyi[i] = (int) keyc[i];
-        
+        int cipher[] = new int[ptext.length()];
+        int decrypt[] = new int[ptext.length()];
+
+        for (int i = 0; i < ptext.length(); i++)
+            ptexti[i] = (int) ptext.charAt(i);
+        for (int i = 0; i < key.length(); i++)
+            keyi[i] = (int) key.charAt(i);
+
+        // the key values are repeated to make it 256 bits
         for (int i = 0; i < 255; i++) {
             s[i] = i;
             k[i] = keyi[i % key.length()];
         }
+
+        // Key Scheduling
         int j = 0;
         for (int i = 0; i < 255; i++) {
             j = (j + s[i] + k[i]) % 256;
@@ -36,6 +37,10 @@ class RC4 {
             s[i] = s[j];
             s[j] = temp;
         }
+
+        // Stream generation -> this will generate new key deonted by "z" here
+        // this key will be used for encryption and decryption
+
         int i = 0;
         j = 0;
         int z = 0;
@@ -46,6 +51,9 @@ class RC4 {
             s[i] = s[j];
             s[j] = temp;
             z = s[(s[i] + s[j]) % 256];
+
+            // Encrryption = newKey ^ PT
+            // decryption = newKey ^ CT
             cipher[l] = z ^ ptexti[l];
             decrypt[l] = z ^ cipher[l];
         }
